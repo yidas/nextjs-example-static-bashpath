41b1f3f1dfc1b9ad03e0913bb33e835dc07effc2
Nick Tsai <myintaer@gmail.com>
2025-04-19 23:38:42 +0800

    Set the nextConfig for static export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export', // Exports the app as static HTML files
+    trailingSlash: true, // Adds a trailing slash to the end of each route (e.g., /about/)
+    basePath: '/_github/nextjs-example-static-bashpath/out', // Your desired path prefix
+}
 
 module.exports = nextConfig
